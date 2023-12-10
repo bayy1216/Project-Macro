d29d357898efa40fcd32ab456343b9f5e980b029
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-12-10 15:55:34 +0900

    [Feat]: flutter 폴더 생성

--- flutter_folder_feat.py
+++ flutter_folder_feat.py
@@ -13,14 +13,37 @@ def create_folder(folder_path):
     except Exception as e:
         print(f"폴더 생성 실패: {e}")
 
+
+def create_file(file_path, content):
+    with open(file_path, "w") as file:
+        # 파일에 쓸 내용 작성
+        file.write(content)
+
+
+def screen_text(feat):
+    txt = (f"import 'package:flutter/material.dart';\n\nclass {feat.capitalize()}Screen extends StatelessWidget {{\n"
+           f"  const {feat.capitalize()}Screen({{Key? key}}) : super(key: key);\n\n"
+           f"  @override\n  Widget build(BuildContext context) {{\n"
+           f"    return const Container();\n  }}"
+           f"\n}}")
+    return txt
+
+
 def create_feat(feat):
     create_folder(f"./{feat}")
-    folders = ["component", "model", "provider","repository","view"]
+    folders = ["component", "model", "provider", "repository", "view"]
     for f in folders:
-        create_folder(f"./{feat}/{f}")
+        folder_name = f"./{feat}/{f}"
+        create_folder(folder_name)
+        if f == "repository":
+            create_file(folder_name + f"/{feat}_repository.dart", f"class {feat.capitalize()}Repository {{\n\n}}")
+        elif f == "view":
+            create_file(folder_name + f"/{feat}_screen.dart", screen_text(feat))
+
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
+    print("생성할 feat를 입력해주세요(소문자) : ", end="")
+    args = input().split()
 
     # 폴더 생성 함수 호출
     for feats in args:
